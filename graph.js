6426607f5db203f162daeb10c1257f5c3933700f
Ian Isaacson <ian_isaacson@yahoo.com>
2025-07-03 12:20:12 -0700

    Updated addVertices to conform to DRY.

--- graph.js
+++ graph.js
@@ -18,15 +18,23 @@ class Graph {
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
   addVertices(vertexArray) {
     for (const vertex of vertexArray) {
-      this.nodes.add(vertex);
+      this.addVertex(vertex);
     }
   }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    // if (this.nodes.has(v1 && this.nodes.has(v2)) {
+    //   this.nodes
+    //   this.nodes.delete()
+    // }
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    // if (this.nodes.has(v1 && this.nodes.has(v2)) {
+    // }
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
   removeVertex(vertex) {}
