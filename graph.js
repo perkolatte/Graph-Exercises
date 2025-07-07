be5e18bcf4536b7082353655efd77237c262d2df
Ian Isaacson <ian_isaacson@yahoo.com>
2025-07-06 19:27:18 -0700

    Implemented breadthFirstSearch. Exercise complete.

--- graph.js
+++ graph.js
@@ -74,7 +74,29 @@ class Graph {
   }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    let visitedValues;
+    if (this.nodes.has(start)) {
+      visitedValues = [];
+      let current = null;
+      const toVisit = [start];
+      const seen = new Set();
+      seen.add(start);
+
+      while (toVisit.length) {
+        current = toVisit.shift();
+        seen.add(current);
+        visitedValues.push(current.value);
+        for (const adjacent of current.adjacent) {
+          if (!seen.has(adjacent)) {
+            toVisit.push(adjacent);
+            seen.add(adjacent);
+          }
+        }
+      }
+    }
+    return visitedValues;
+  }
 }
 
 module.exports = { Graph, Node };
